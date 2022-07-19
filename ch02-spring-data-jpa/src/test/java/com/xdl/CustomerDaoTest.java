53e2be1b5a3ed5b43ac3f7c04c37f8a94780ac44
xdl <13333759329@163.com>
2022-07-20 01:18:59 +0800

    使用接口中提供的方法进行数据库操作

--- ch02-spring-data-jpa/src/test/java/com/xdl/CustomerDaoTest.java
+++ ch02-spring-data-jpa/src/test/java/com/xdl/CustomerDaoTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 
 @RunWith(SpringJUnit4ClassRunner.class) // 声明spring提供的单元测试环境
 @ContextConfiguration(locations = "classpath:applicationContext.xml") // 指定spring容器的配置信息
@@ -17,13 +19,14 @@ public class CustomerDaoTest {
 
     @Test
     public void testFindOne() {
-        Customer customer = customerDao.findOne(1L);
+        Customer customer = customerDao.findOne(2L);
         System.out.println(customer);
     }
 
     /**
      *  save：保存或更新，
-     *      根据传递的对象是否存在id，如果没有id属性则保存，否则为更新
+     *      根据传递的对象是否存在id，如果没有id属性则保存，
+     *      如果存在id则先根据id进行查询，如果存在则更新，如果不存在则创建
      */
     @Test
     public void testSave() {
@@ -43,5 +46,35 @@ public class CustomerDaoTest {
     @Test
     public void testDelete() {
         customerDao.delete(1L);
+    }
+
+    /**
+     * 查询所有
+     */
+    @Test
+    public void testFindAll() {
+        List<Customer> customers = customerDao.findAll();
+        for (Customer customer: customers) {
+            System.out.println(customer);
+        }
+    }
+    /**
+     * 测试统计查询，查询客户的总数量
+     */
+    @Test
+    public void testCount() {
+        Long count = customerDao.count(); // 查询数量
+        System.out.println(count);
+    }
+
+    /**
+     * 测试: 判断id为4的客户是否存在
+     *      1、可以通过查询id为4的客户，如果为空代表不存在
+     *      2、判断数据中id为4的客户数量，为0表示不存在(exists方法使用的方法)
+     */
+    @Test
+    public void testExists() {
+        boolean exists = customerDao.exists(4l);
+        System.out.println(exists);
     }
 }
